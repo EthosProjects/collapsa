f08d3eeeebf4fae009bb1dd94c35117f5b98d383
Logos-King <logospiercing@gmail.com>
2020-06-10 18:08:28 -0400

    Fixed ban and kick command

--- collapsabot/commands/kick.js
+++ collapsabot/commands/kick.js
@@ -23,12 +23,23 @@ module.exports = {
         // Check if a reason has been given by the message author
         let authorRolesArr = [...author.roles.cache.values()].map(role => role.rawPosition).sort((a, b) => b - a)
         let memberRolesArr = [...author.roles.cache.values()].map(role => role.rawPosition).sort((a, b) => b - a)
-        if(authorRolesArr[0] <= memberRolesArr[0]) return message.reply('You can\'t kick this user your role is not high enough')
-        if (!author.hasPermission(['KICK_MEMBERS'])) return message.reply('You can\'t kick this user because you don\'t have sufficient pemissions')
         if (!guild.member(user).kickable) return message.reply('You can\'t kick this user because the bot does not have sufficient permissions!'); 
         if (!guild.owner.id == user.id) return message.reply('You can\'t kick this user because they own the server')
-        // Check if the user is bannable with the bot's permissions
-        await member.kick() // Bans the user
+        if (guild.owner.Kicked == author.id || author.id == client.owner){
+            await member.kick()
+            let embed = new MessageEmbed()
+                .setColor('#ff0000')
+                .setTitle(`Kicked ${message.mentions.users.first().username}`)
+                .addField('Reason', reason ? reason : 'No reason specified')
+                .setDescription('Owner force')
+                .setAuthor(message.author.username, message.author.avatarURL())
+            message.channel.send(embed)
+            return
+        }
+        if(authorRolesArr[0] <= memberRolesArr[0]) return message.reply('You can\'t kick this user your role is not high enough')
+        if (!author.hasPermission(['KICK_MEMBERS'])) return message.reply('You can\'t kick this user because you don\'t have sufficient pemissions')
+        // Check if the user is kickable with the bot's permissions
+        await member.kick() // Kicks the user
         console.log('rip')
         let embed = new MessageEmbed()
             .setColor('purple')
