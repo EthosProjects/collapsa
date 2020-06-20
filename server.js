8e7e2b480b2541b4c5f2903da5cacb733e80a527
Logos-King <logospiercing@gmail.com>
2020-06-20 11:47:13 -0400

    fix pls

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ let toLiteral = obj => JSON.parse(JSON.stringify(obj))
 const { mongodbInteractor } = require('./mongoDB')
 const mongoDB = new mongodbInteractor('LogosKing', 'TBKCKD6B')
 // Run separate https server if on localhost
-
+/*
 if (process.env.NODE_ENV == 'development') {
     httpsServer = https.createServer(httpsOptions, app).listen(process.env.PORT, function () {
         console.log("Express server listening with https on port %d in %s mode", this.address().port, app.settings.env);
@@ -97,15 +97,15 @@ if (process.env.NODE_ENV == 'development') {
         }
     });
 } else {
-    /*app.use(function (req, res, next) {
+    app.use(function (req, res, next) {
         //res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
         if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
             return next() /*res.redirect(301, 'https://' + req.host + req.url);
         } else {
             return next();
         }
-    });*/
-};
+    });
+};*/
 //var httpsServer = http.Server(app);
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt')
